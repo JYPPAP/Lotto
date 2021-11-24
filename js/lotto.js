ac759fceee9938b34d031440e8401ed656647fb4
JYPPAP <yungsu222@naver.com>
2021-11-24 18:23:21 +0900

    HTML 1. 슬라이드 임시 텍스트 삭제 CSS 1. 슬라이드 큰 텍스트 위치 조정 JS 1.  엄격모드 주석처리  - 일부 기능이 IE9에서 제한이 생기는 것으로 확인. 2. 가독성 향상을 위해 일부분 간격 조정 3. 함수명을 명확하게 하기위해 일부 함수명 변경 4. IE9에서 classList가 호환되지 않아서 className ="lottoNumber 색상Num";으로 변경 5. Ajax 완성  - 전부 배열로 받와서서 처리.  - xhr.response도 IE9에서 호환되지 않아서 responseText로 변경.  - 읽어온 값을 for 문을 통해서 전제적용. 기타 1. 테스트용 eslintrc 생성 2. node-modules "무시" 처리

--- js/lotto.js
+++ js/lotto.js
@@ -1,5 +1,4 @@
-"use strict";
-
+// "use strict";
 document.addEventListener("DOMContentLoaded", function () {
   /*###################
         번호 추천
@@ -15,31 +14,33 @@ document.addEventListener("DOMContentLoaded", function () {
   var year = today.getFullYear();
   var month = ('0' + (today.getMonth() + 1)).slice(-2);
   var day = ('0' + (today.getDate())).slice(-2);
+  
   todayShow.textContent = "("+year+". "+month+". "+day+")";
 
   /* 로또 숫자배열 생성 */
   for (var i = 0; i < 45; i++) {totalNumber[i] = i + 1;}
 
   checkBtn.onclick = function() {
+    /* 연속클릭 방지 */
     if(clickFlag) return true;
     lottoNumber.map(function (num) {
       num.textContent = "";
       num.style.visibility = "hidden";
     });
     clickFlag = true;
-    return clickFlag, randomNumber();
+    return clickFlag, selectNumber();
   }
-  /* 배열에서 무작위의 숫자 7개를 추출하기 */
-  function randomNumber() {
+  
+  function selectNumber() {
     var randomArray = [];
     for (var i = 0; i < 7; i++) {
       var randomNum = Math.floor(Math.random() * 45) + 1;
       randomArray[i] = randomNum;
     }
-    return checkNum(randomArray);
+    return checkNumber(randomArray);
   }
 
-  function checkNum (randomArray) {
+  function checkNumber (randomArray) {
     /* 중복 확인 */
     var filterArray = randomArray.filter(function(item, index) {
       if (randomArray.indexOf(item) == index)
@@ -47,12 +48,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     if (filterArray.length !== 7) {
       console.log("중복: "+filterArray);
-      return randomNumber();
+      return selectNumber();
     } else {
       console.log("통과: "+filterArray);
       return sortNumber(filterArray);
     }
   }
+
   function sortNumber(filterArray) {
     var lastNum = filterArray.pop();
     var sortArray = filterArray.sort(function(a, b) {
@@ -62,6 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     return setNumber(lastNum, sortArray);
   }
+
   function setNumber(lastNum, sortArray) {
     for (var i = 0; i < sortArray.length; i++) {
       lottoNumber[i].textContent = sortArray[i];
@@ -71,21 +74,23 @@ document.addEventListener("DOMContentLoaded", function () {
     checkColor();
     return showNumber();
   }
+
   function checkColor() {
     lottoNumber.map(function (num) {
       if (num.textContent < 10) {
-        num.classList.add("yellowNum");
+        num.className = "lottoNumber yellowNum";
       } else if (num.textContent < 20) {
-        num.classList.add("blueNum");
+        num.className = "lottoNumber blueNum";
       } else if (num.textContent < 30) {
-        num.classList.add("redNum");
+        num.className = "lottoNumber redNum";
       } else if (num.textContent < 40) {
-        num.classList.add("greenNum");
+        num.className = "lottoNumber greenNum";
       } else {
-        num.classList.add("grayNum");
+        num.className = "lottoNumber grayNum";
       }
     });
   }
+
   function showNumber() {
     for (var i=0; i<lottoNumber.length; i++) {
       var showCount = 0;
@@ -110,6 +115,10 @@ document.addEventListener("DOMContentLoaded", function () {
   /*###################
         Ajax
   ###################*/
+  var epiNum = Array.prototype.slice.call(document.getElementsByClassName("episodeNumber"));
+  var rank1 = Array.prototype.slice.call(document.getElementsByClassName("rank1"));
+  var rank2 = Array.prototype.slice.call(document.getElementsByClassName("rank2"));
+  var rank3 = Array.prototype.slice.call(document.getElementsByClassName("rank3"));
   var xhr = new XMLHttpRequest();
 
   xhr.open('GET', 'json/lotto.json');
@@ -121,31 +130,17 @@ document.addEventListener("DOMContentLoaded", function () {
     if (xhr.readyState !== XMLHttpRequest.DONE) return;
 
     if (xhr.status === 200) {
-
-      // Deserializing (String → Object)
-      // var responseObject = JSON.parse(xhr.responseText);
-      // var rArray = JSON.stringify(responseObject);
-      // console.log("responseObject의 TYPE: "+typeof(responseObject));
-      console.log("###############");
-      console.log(xhr);
-      var valueee = JSON.parse(xhr.response);
-      console.log("typeof(valueee)의 TYPE: "+typeof(valueee));
-      console.log("");
-      console.log("valueee");
-      console.log(valueee);
-      console.log("");
-
-      console.log("");
-      console.log("valueee[875]");
-      console.log(valueee[875]);
-      console.log("");
-      console.log('valueee[875]["3st"]');
-      console.log(valueee[875]["3st"]);
-      console.log("###############");
-      
-      // JSON → HTML String
-
-      // document.getElementById('content').innerHTML = newContent;
+      var parseValue = JSON.parse(xhr.responseText);
+      var kValue = Object.keys(parseValue);
+      var vValue = Object.keys(parseValue).map(function(ele) {
+        return parseValue[ele]
+      });
+      for (var i=0; i<kValue.length; i++) {
+        epiNum[i].textContent = kValue[(kValue.length-1)-i]+"회 추첨번호 적중!";
+        rank1[i].textContent = "1등 : "+vValue[(kValue.length-1)-i]['1st']+"건";
+        rank2[i].textContent = "2등 : "+vValue[(kValue.length-1)-i]['2st']+"건";
+        rank3[i].textContent = "3등 : "+vValue[(kValue.length-1)-i]['3st']+"건";
+      }
     } else {
       console.log("["+xhr.status+"] : "+xhr.statusText);
     }
